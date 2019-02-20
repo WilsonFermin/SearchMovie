c9dd54ca92bf3216e9551e3d1a3879ad69f0049f
Wilson Antonio Ruiz Fermin <wilsonruiz98@hotmail.com>
2019-02-19 20:32:59 -0400

    Aplicando todos los repositorios

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'; 
+import { Switch, Route } from 'react-router-dom'
+
+import {Home} from './Pages/Home';
+import {Detail} from './Pages/Detail';
+import {NotFound  } from './Pages/NotFound';
+
+import './App.css';
+import 'bulma/css/bulma.css';
+
+
+class App extends Component {
+ 
+  render() {
+    return (
+      <div className="App">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/detail/:movieId' component={Detail} />
+            <Route component={NotFound} />
+          </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
